d496d8fd8e796a444ee5fe292365428975b84319
NURUL HUSSAIN <166852314+nurulhussain9457@users.noreply.github.com>
2026-05-10 19:46:29 +0530

    Rename S3 bucket resource from omkar to nurul

--- Module-9 Terraform – Infrastructure as Code/main.tf
+++ Module-9 Terraform – Infrastructure as Code/main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "omkar_bucket" {
-  bucket = "omkar-terraform-bucket-2821"
+resource "aws_s3_bucket" "nurul_bucket" {
+  bucket = "nurul-terraform-bucket-2821"
 }
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0a14f53a6fe4dfcd1"
